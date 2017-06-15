100bfa08414d99fbfa336059cf82daa988d68c68
linux <Yingth>
2017-06-14 18:04:00 -0700

    IBXCHDBC MOGIFIED
    	modified:   .gitignore
    	modified:   stu.c

--- stu.c
+++ stu.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    printf("hello world");
-    
-    printf("hello world");
-    printf("hello world");
-    printf("hello world");
-    printf("hello world");
-    printf("hello world");
     printf("welcome to beijing\n");
     return 0;
 }
